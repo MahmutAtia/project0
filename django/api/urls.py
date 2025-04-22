333810d12fd1fbf16261fb9877a0b4a5e8d5d409
Mahmut Atia <mahmoud.atia@turboard.com>
2025-04-22 07:28:54 +0300

    #Feat101 did the backend part but getiing too many problems with yaml i want to try json

--- django/api/urls.py
+++ django/api/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('resumes/generate_website_yaml/', views.generate_personal_website_bloks, name='generate-resume-website-bloks'),
     path('resumes/generate_from_job_desc/', views.generate_from_job_desc, name='generate-resume-from-job-desc'),
     path('resumes/edit/', views.generate_resume_section, name='edit-resume'),
+    
+    # gererate document
+    path('resumes/generate_document/', views.generate_document_bloks, name='generate-document'),
+
 
     # editor views here
     path('website-yaml/<uuid:resume_id>/', views.get_website_yaml_json, name='serve_website_yaml_json'),
@@ -28,8 +32,8 @@ urlpatterns = [
     
     
     # 5. Generate document
-    path('resumes/<str:pk>/generate_document/', views.generate_document, name='generate-document'),
-    path('resumes/<str:pk>/document/<uuid:unique_id>/', views.serve_document, name='view-document'),
+    path('resumes/generate_document/', views.generate_document_bloks, name='generate-document'),
+    path('resumes/document/<uuid:unique_id>/', views.get_document_pdf, name='view-document'),
     # path('resumes/<str:pk>/document/<uuid:unique_id>/update/', views.update_document, name='update-document'),
     
 ]
