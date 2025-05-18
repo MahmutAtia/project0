99f309f4e22b6bf5e16e8f845b06a144b7307877
Mahmut Atia <mahmoud.atia@turboard.com>
2025-05-18 07:55:41 +0300

    #fix112 load env from docker

--- django/proj0/settings.py
+++ django/proj0/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 from datetime import timedelta
 
-# if the .env file loaad it
-if Path(".env").exists():
-    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -229,7 +225,7 @@ SIMPLE_JWT = {
 # Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT",587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
