9c9da3ffa34e921b89ed41997f8d879c31bf297e
mahmoud <mahmoud.atia.ead@gmail.com>
2025-08-03 09:44:49 +0300

    fix some yaml problems

--- api/features/resumes/prompts.py
+++ api/features/resumes/prompts.py
@@ -29,9 +29,10 @@ No Fabrication: Do not invent information but enhance and present the candidate'
 
 Simple, Readable, and Impactful Language: Use clear, concise, and professional language throughout.
 
-PAY ATTENTION, Quote all strings in the yaml output with double quotes. Use | for multiline strings and escape ':' s in the yaml output.
+PAY ATTENTION, Quote all strings in the yaml output with double quotes. Use | for multiline strings the yaml output.
 PAY ATTENTION to all yaml parsing rules and indentation.
 When using "'" it must escaped with a "''" like this: "I''m a software engineer"
+Do not escape special characters inside quotes.
 Do not output any yaml comments in the output.
 
 Output all in the {language} language.
---
+++ api/features/resumes/yml_rules.py
@@ -0,0 +1,6 @@
+
+PAY ATTENTION, Quote all strings in the yaml output with double quotes. Use | for multiline strings the yaml output.
+PAY ATTENTION to all yaml parsing rules and indentation.
+When using "'" it must escaped with a "''" like this: "I''m a software engineer"
+Do not escape special characters inside quotes.
+Do not output any yaml comments in the output.
